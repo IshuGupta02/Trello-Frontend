20708156cad29347de0857a7c425d32a6cb77211
IshuGupta02 <ishugupta0298@gmail.com>
2021-09-28 19:25:45 +0530

    Implemented automatic login

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -20,16 +20,12 @@ class Dashboard extends Component{
             <div>
             Hello Dashboard
             </div>
-            
-            
         );
     }
 
     async componentDidMount(){
 
         const user1= await axios.get('http://127.0.0.1:8000/api/user/info/', {withCredentials:true}).then(console.log("done"));
-
-
         console.log(user1.data);
         
     }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,23 +1,53 @@
 import React, {Component} from 'react';
 import { render } from '@testing-library/react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 class HomePage extends Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.state = { 
+            loggedIn :"False",
+        };
+    }
+
+    renderRedirect= () =>{
+        if(this.state.loggedIn==="True")
+        {
+            return <Redirect to={{pathname:'../dashboard'}}/>
+        }
+    }
 
     render(){
         return(
             <div>
+                {this.renderRedirect()}
                 <a href="https://internet.channeli.in/oauth/authorise/?client_id=z1T5401eydvctrKve1qOJpYBGdTrWSaMZWhAe98j&redirect_uri=http://127.0.0.1:3000/login&state=done">Login with Omniport</a>
 
             </div>
-            
-            
-
-//             /oauth/authorise/?client_id=MY_CLIENT_ID
-//   <&redirect_uri=REDIRECT_URI>
-//   <&state=RANDOM_STATE_STRING>
 
         );
+    }
+
+    async componentDidMount(){
+
+        try{
+            const response= await axios({url:'http://127.0.0.1:8000/api/user/info/' ,method:'GET',withCredentials:true} ).then(console.log("done"));
+
+            console.log(response.status)
+    
+            if(response.status==202){
+                // console.log("changing state")
+                this.setState({loggedIn:"True"});
+                // console.log(this.state.error);
+            }
+        }
+        catch(e){
+
+        }
+
     }
 }
 
